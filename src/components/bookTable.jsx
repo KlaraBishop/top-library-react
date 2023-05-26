ed6b019d1382e2023cde5faa1dfb5cc07a3477c1
KleraStretch <stretch068@protonmail.com>
2023-05-26 00:50:53 -0700

    implemented delete buttons to table, along with axios statements to propogate deletion to db

--- src/components/bookTable.jsx
+++ src/components/bookTable.jsx
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 const BookTable = (props) => {
     BookTable.propTypes = {
       bookList: PropTypes.array.isRequired,
+      deleteClick: PropTypes.func.isRequired,
     };
+
+    const onDeleteClick = (id) => {
+        props.deleteClick(id)
+    }
   
     return <table className='book-table'>
       <thead>
@@ -11,14 +16,16 @@ const BookTable = (props) => {
           <th>Name</th>
           <th>Author</th>
           <th>Game</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
         {props.bookList.map(book => {
-          return <tr key={book.name}> 
+          return <tr key={book.id}> 
               <td>{book.name}</td>
               <td>{book.author}</td>
               <td>{book.game}</td>
+              <td id="delete-button" onClick={() => onDeleteClick(book.id)}>Delete</td>
             </tr>
         })}
       </tbody>
