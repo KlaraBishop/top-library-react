5661633bfdfa4ac970c3ae9768aec548b0cc0ef4
KleraStretch <stretch068@protonmail.com>
2023-05-25 23:26:53 -0700

    Refactored the form to add a new book into it's own component file

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,19 @@ import './stylesheets/App.css'
 import library from './data/library.json'
 import { useState } from 'react';
 import BookTable from './components/bookTable';
-
-const CreateBook = (name, author, game) => {
-  return { name, author, game }
-}
+import AddBookForm from './components/addBookForm';
 
 function App() {
   const [BookList, setBookList] = useState(library);
 
-  const [BookName, setBookName] = useState('');
-  const [Author, setAuthor] = useState('');
-  const [Game, setGame] = useState('');
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-
-    setBookList(() => [...BookList, CreateBook(BookName, Author, Game)])
-    setBookName('')
-    setAuthor('')
-    setGame('')
+  const addBook = (book) => {
+    setBookList(() => [...BookList, book])
   }
 
-  return (
-    <>
+  return <>
       <BookTable bookList={BookList} />
-      <form>
-        <label>
-          Book:
-          <input id='bookName' type='text' value={BookName} onChange={e => setBookName(e.target.value)} />
-        </label>
-        <label>
-          Author:
-          <input id='author' type='text' value={Author} onChange={e => setAuthor(e.target.value)} />
-        </label>
-        <label>
-          Book:
-          <input id='game' type='text' value={Game} onChange={e => setGame(e.target.value)} />
-        </label>
-        <input id='submit' type='submit' value={'Add Book'} onClick={e => onSubmit(e)}/>
-      </form>
+      <AddBookForm addBook={addBook} />
     </>
-  )
 }
 
 export default App
