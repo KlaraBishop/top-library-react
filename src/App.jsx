10b983b3df6a1a2e75bbc1b6c314699fe486e6d0
KleraStretch <stretch068@protonmail.com>
2023-05-26 00:14:32 -0700

    implemented Axios, added post satement to allow data to remain even after reload

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import './stylesheets/App.css'
 import { useEffect, useState } from 'react';
 import BookTable from './components/bookTable';
 import AddBookForm from './components/addBookForm';
+import axios from 'axios';
 
 function App() {
   const [BookList, setBookList] = useState([]);
 
   const addBook = (book) => {
     setBookList(() => [...BookList, book])
+
+    axios.post('http://localhost:3000/books', book)
+    .then(response => console.log(response))
   }
 
   useEffect (() => {
-    fetch('http://localhost:3000/books')
-    .then(response => response.json())
-    .then(data => setBookList(data))
+    axios.get('http://localhost:3000/books')
+    .then(response => setBookList(response.data))
   },[])
 
   return <>
