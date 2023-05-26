16dc38965ea7aae76a56b3ddc0a5fc771633d527
KleraStretch <stretch068@protonmail.com>
2023-05-26 00:04:31 -0700

    Implemented JSON-server to serve as a temporary backend and refactored json file to serve as db

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './stylesheets/App.css'
-import library from './data/library.json'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BookTable from './components/bookTable';
 import AddBookForm from './components/addBookForm';
 
 function App() {
-  const [BookList, setBookList] = useState(library);
+  const [BookList, setBookList] = useState([]);
 
   const addBook = (book) => {
     setBookList(() => [...BookList, book])
   }
+
+  useEffect (() => {
+    fetch('http://localhost:3000/books')
+    .then(response => response.json())
+    .then(data => setBookList(data))
+  },[])
 
   return <>
       <BookTable bookList={BookList} />
